1fc943fb6bb437886aa79d2789c58d9f5400d44c
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-16 22:57:12 +0100

    added jsdoc

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,13 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * Gicne a number, returns a negative version of the number.
+ * @param {number} n - input number 
+ * @returns {number} `n` in negative form
+ */
 function asNegative(n) {
   // ...
 }
 
+
+console.log(asNegative(10), 'output should be, -10');
 
